fda435e3da8a9501fbcac4410ee0c9713a70e651
Nutubit17 <koobonhoo@gmail.com>
2024-06-12 17:53:17 +0900

    feat : folder and core script import

---
+++ Assets/Kbh/01Scripts/Logic/LogicManager_Kbh.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicManager_Kbh : MonoSingleton<LogicManager_Kbh>
+{
+   // Requiring Components in here,
+   // 
+   // Player
+   // Exe file list
+   // 
+
+}
